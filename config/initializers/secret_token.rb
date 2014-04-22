43ec79443500a51404ff22280f83daa3042a018b
Andrew Deitrick <ajdeitrick@gmail.com>
2014-04-22 10:41:40 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Motivator::Application.config.secret_key_base = 'f77840973379c48b71d9fe4c83d70bb13d97ce5929aec572118284b3bea8f4d8f1404b32706ae4ce9898580aa1c91c9a9619bc8d6a26018ac24525ac7af39e3a'
